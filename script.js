ae63d1e1e06f7757d95b03d88815b62e0a74dec4
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2026-01-01 18:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+let textarea = document.getElementById("evaluatedText");
+let wordCount = document.getElementById("wordCount");
+
+textarea.addEventListener("input", function () {
+  const str = textarea.value.trim();
+
+  if (str === "") {
+    wordCount.innerText = 0;
+  } else {
+    wordCount.innerText = str.split(/\s+/).length;
+  }
+});
+
+
